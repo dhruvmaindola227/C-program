5f24f27a3bf0d077c3a7b21bdae8079498088511
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-06 20:33:45 +0530

    added comments to selection sort

--- Q1F.c
+++ Q1F.c
@@ -18,7 +18,8 @@ int main(){
         printf("%d " , arr[i]);
     }
 
-
+    //basic logic is to find the maximum number and store it in the last index.
+    //then search in the remaining indices and repeat the same process.
     for(int i = 0 ; i < n ; i++){
         max = 0;
     for(int j = 0 ; j < n - i ; j++){
