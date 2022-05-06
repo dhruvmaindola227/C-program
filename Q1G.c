04440bc0f2d259d1c5d63257c51bb8b131ea658c
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-06 19:32:52 +0530

    bubble sort added.

--- Q1G.c
+++ Q1G.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
-//insertion sort.
+//bubble sort
 int main(){
     int n;
     printf("Enter the size of the array");
     scanf("%d" , &n);
     int arr[n];
     printf("Enter the elements");
-    for(int i = 0 ; i < 5 ; i++){
+    for(int i = 0 ; i < n ; i++){
         scanf("%d" , &arr[i]);
     }
     int temp;
     printf("before sorting\n");
-    for(int i = 0 ; i < 5 ; i++){
+    for(int i = 0 ; i < n ; i++){
         printf("%d " , arr[i]);
     }
-    for(int i = 1 ; i < 5 ; i++){
-        temp = arr[i];
-        int j = i - 1;
-        while (j >=0 && arr[j] > temp){
-            arr[j+1] = arr[j];
-            j--;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 1 ; j < n - i ; j++){
+             if(arr[j-1] > arr[j]){
+                temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+             }   
         }
-        arr[j+1] = temp;
     }
 
     printf("\nafter sorting\n");
-    for(int i = 0 ; i < 5 ; i++){
+    for(int i = 0 ; i < n ; i++){
         printf("%d " , arr[i]);
     }
     
