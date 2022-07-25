625f8ea3f3f76cd39d52bd03e6a15765e8e92123
Dhruv Maindola <maindola.dhruv@gmail.com>
2022-07-25 15:37:23 +0530

    stack program added

--- DFS LAB 3/Stack/StackUsingArrays.c
+++ DFS LAB 3/Stack/StackUsingArrays.c
@@ -1,1 +1,91 @@
-// C:\Users\maind\Documents\C program\DFS LAB 3\Queues\DoublyEndedQueueUsingSinglyLinkedList.c
+#include<stdio.h>
+int stack[100],choice,size,top,value,i;
+void push(void);
+void pop(void);
+void display(void);
+int main()
+{
+    top=-1;
+    printf("Enter the size of STACK[MAX=100]:");
+    scanf("%d",&size);
+    printf("STACK OPERATIONS USING ARRAY");
+    printf("--------------------------------\n");
+    printf(" 1.PUSH\n 2.POP\n 3.DISPLAY\n 4.EXIT");
+    do
+    {
+        printf("\n Enter the Choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            {
+                push();
+                break;
+            }
+            case 2:
+            {
+                pop();
+                break;
+            }
+            case 3:
+            {
+                display();
+                break;
+            }
+            case 4:
+            {
+                printf("\n\t EXIT POINT ");
+                break;
+            }
+            default:
+            {
+                printf ("\n\t Please Enter a Valid Choice(1/2/3/4)");
+            }
+                
+        }
+    }
+    while(choice!=4);
+    return 0;
+}
+void push()
+{
+    if(top>=size-1)
+    {
+        printf("\n\tSTACK is over flow");
+        
+    }
+    else
+    {
+        printf(" Enter a value to be pushed:");
+        scanf("%d",&value);
+        top++;
+        stack[top]=value;
+    }
+}
+void pop()
+{
+    if(top<=-1)
+    {
+        printf("\n\t Stack is under flow");
+    }
+    else
+    {
+        printf("\n\t The popped elements is %d",stack[top]);
+        top--;
+    }
+}
+void display()
+{
+    if(top>=0)
+    {
+        printf("\n The elements in STACK \n");
+        for(i=top; i>=0; i--)
+            printf("\n%d",stack[i]);
+        printf("\n Press Next Choice");
+    }
+    else
+    {
+        printf("\n The STACK is empty");
+    }
+   
+}
